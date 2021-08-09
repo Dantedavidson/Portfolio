9333f144d38e4f85c19be78d84d9e700bba7e2d7
Dantedavidson <50653094+Dantedavidson@users.noreply.github.com>
2021-08-10 09:34:36 +1000

    theme set up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import GlobalStyles from "../styles/GlobalStyles"
+import { ThemeProvider } from "styled-components"
+import theme from "../styles/Theme.json"
 
 export default function Home() {
-  return <div></div>
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+    </ThemeProvider>
+  )
 }
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components"
+
+export default createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&family=STIX+Two+Text:wght@400;600;700&display=swap');
+   
+*{
+    box-sizing:border-box;
+    margin:0;
+    padding:0;
+}
+
+body{
+    background-color: ${({ theme }) => theme.background};
+    color:${({ theme }) => theme.textLight}
+}
+`
